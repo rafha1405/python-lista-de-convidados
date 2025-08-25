aac6657fdb54ef0617f85d8d8f40d5a91579d84e
rafha1405 <rafaela.raissa.souza@escola.pr.gov.br>
2025-08-25 09:40:22 -0300

    criei as listas

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 # Lista de convidados que foram convidados para sua festa
+convidados_da_festa = ['nayelli','Igor','gabriel','bianca','daniela'
+
+# dicionário vazio para armazenar o status de presença de cada pessoa
+status_presenca = {}
+# Esta é a lista de pessoas que tentaram entrar na festa
+lista_de_chegadas = ['igor','gabriel','bianca']
